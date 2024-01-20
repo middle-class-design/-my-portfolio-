694077e29c6210705fce3d25d85ac30e48c02224
middle-class-design <rahul1302007@gmail.com>
2024-01-20 19:09:53 +0530

    updated

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
 
 
 
-function openCity(evt, cityName) {
+function opentab(evt, TabName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -34,7 +34,7 @@ function openCity(evt, cityName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(TabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
 
